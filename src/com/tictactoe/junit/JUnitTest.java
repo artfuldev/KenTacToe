9264a284d34abac3d9fc6ca91f51c728de011a92
Kenshin Himura <kenshin.thebattosai@live.com>
2013-01-10 18:59:22 +0530

    JUnit Test

--- src/com/tictactoe/junit/JUnitTest.java
+++ src/com/tictactoe/junit/JUnitTest.java
@@ -11,17 +11,17 @@ public class JUnitTest {
 	@Test
 	public void test() {
 		Table t=new Table(3,3);
-		t.updateTable(0,'P');
+		t.updateTable(0,'X');
 		t.updateTable(1,'X');
 		t.updateTable(2,'X');
 		t.updateTable(3,'X');
-		t.updateTable(4,'P');
-		//t.updateTable(5,'P');
-		//t.updateTable(6,'X');
-		//t.updateTable(7,'X');
-		t.updateTable(8,'P');
+		t.updateTable(4,'X');
+		t.updateTable(5,'X');
+		t.updateTable(6,'X');
+		t.updateTable(7,'X');
+		t.updateTable(8,'X');
 		System.out.println(t.getScore());
-		assertEquals("Done",900,t.getScore(),1000);
+		assertEquals("Done",-900,t.getScore(),1000);
 		t.printTable();
 	}
 
