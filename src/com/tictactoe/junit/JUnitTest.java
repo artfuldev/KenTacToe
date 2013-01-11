13b03630fd65185a9735820b8caab25d00b368dd
Kenshin Himura <kenshin.thebattosai@live.com>
2013-01-11 11:22:51 +0530

    jUnit Test
    Just tested another set of cases.

--- src/com/tictactoe/junit/JUnitTest.java
+++ src/com/tictactoe/junit/JUnitTest.java
@@ -12,15 +12,16 @@ public class JUnitTest {
 	public void test() {
 		Table t=new Table(3,3);
 		t.updateTable(0,'O');
-		t.updateTable(1,'X');
+		t.updateTable(1,'O');
 		t.updateTable(2,'X');
-		//t.updateTable(3,'X');
+		//t.updateTable(3,'O');
 		t.updateTable(4,'O');
-		t.updateTable(5,'O');
-		t.updateTable(6,'O');
+		//t.updateTable(5,'O');
+		t.updateTable(6,'X');
 		t.updateTable(7,'X');
 		t.updateTable(8,'X');
 		System.out.println(t.getScore());
+		System.out.println(t.isComplete());
 		assertEquals("Done",-4000,t.getScore(),10000);
 		t.printTable();
 	}
